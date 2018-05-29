ad27479270d5d8b078cba5b7ecdc5959d38e07fc
Abhishek Muthyam <34750702+abhishekmuthyam@users.noreply.github.com>
2018-05-29 16:31:59 +0530

    Update PCF_CloudService_Script.bat

--- PCF_CloudService_Script.bat
+++ PCF_CloudService_Script.bat
@@ -1,21 +1,31 @@
 @ECHO OFF
 FOR /F "tokens=1,2 delims==" %%A IN (C:/Users/muthyama/build/%DEPLOY_ENV%_PCF.properties) DO (
-    IF "%%A"=="API_URL" SET PCF_ENDPOINT_NAME=%%B 
+    IF "%%A"=="API_URL" SET API_URL=%%B 
 )
 FOR /F "tokens=1,2 delims==" %%A IN (C:/Users/muthyama/build/%DEPLOY_ENV%_PCF.properties) DO (
-    IF "%%A"=="USER_NAME" SET USER_NAME=%%B
+    IF "%%A"=="USER_NAME" SET USER_NAME=%%B 
 )
 FOR /F "tokens=1,2 delims==" %%A IN (C:/Users/muthyama/build/%DEPLOY_ENV%_PCF.properties) DO (
     IF "%%A"=="PASSWORD" SET PASSWORD=%%B
 )
 FOR /F "tokens=1,2 delims==" %%A IN (C:/Users/muthyama/build/%DEPLOY_ENV%_PCF.properties) DO (
-    IF "%%A"=="ORGANIZATION" SET PCF_ORG=%%B
+    IF "%%A"=="ORGANIZATION" SET ORGANIZATION=%%B
 )
 FOR /F "tokens=1,2 delims==" %%A IN (C:/Users/muthyama/build/%DEPLOY_ENV%_PCF.properties) DO (
-    IF "%%A"=="SPACE" SET PCF_SPACE=%%B
+    IF "%%A"=="SPACE" SET SPACE=%%B
 )
-ECHO "PCF_ENDPOINT_NAME: %PCF_ENDPOINT_NAME%"
-ECHO "USER NAME: %USER_NAME%"
-ECHO "PASSWORD: %PASSWORD%"
-ECHO "PCF_ORG: %PCF_ORG%"
-ECHO "PCF_SPACE: %PCF_SPACE%"
+
+ECHO "API_URL=%API_URL%"
+ECHO "USER_NAME=%USER_NAME%"
+ECHO "PASSWORD=%PASSWORD%"
+ECHO "ORGANIZATION=%ORGANIZATION%"
+ECHO "SPACE=%SPACE%"
+
+ECHO API_URL="%API_URL%">env.properties
+ECHO USER_NAME="%USER_NAME%">>env.properties
+ECHO PASSWORD="%PASSWORD%">>env.properties
+ECHO ORGANIZATION="%ORGANIZATION%">>env.properties
+ECHO SPACE="%SPACE%">>env.properties
+
+copy env.properties /B+ ,,/Y
+echo "properties files copied"
